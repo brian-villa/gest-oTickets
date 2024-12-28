b001776e65d046d2cf12cb3703d2fda6a4108186
brian-villa <brian.rj99@yahoo.com.br>
2024-12-28 15:07:25 +0000

    SQL injection, helmet and csurf protection, loginUser method and refactor userController

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -1,9 +1,14 @@
 const User = require("../models/users");
+const { hashPassword, comparePassword } = require("../utils/pwd");
 
 const createUsers = async (req, res) => {
     try {
-        const newUser = new User(req.body);
+        const { pwd, ...fields } = req.body;
+        const hashedPassword = await hashPassword(pwd);
+
+        const newUser = new User({ ...fields, password: hashedPassword });
         const savedUser = await newUser.save();
+
         res.status(201).json({ message: "User created!", savedUser });
     } catch (e) {
         res.status(500).json({ error: e.message });
@@ -53,10 +58,33 @@ const deleteUsers = async (req, res) => {
     };
 };
 
+const loginUser = async (req, res) => {
+    try{
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+        
+        if(!user) {
+            return res.status(404).json({ error: "User not found "});
+        };
+
+        const isPasswordValid = await comparePassword(password, user.password);
+        if(!isPasswordValid) {
+            return res.status(401).json({ error: "Invalid credentials" });
+        };
+
+        res.status(200).json({ message: "Login successful", user});
+
+    } catch(e) {
+        res.status(500).json({ error: e.message });
+    };
+};
+
 
 module.exports = {
     createUsers,
     getUsers,
     updateUsers,
     deleteUsers,
+    loginUser,
 };
--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,6 @@
 const express = require("express")
+const helmet = require("helmet");
+const csrf = require("csurf");
 const path = require("path")
 
 const db = require('./utils/dbConnect')
@@ -7,10 +9,19 @@ const apiRoutes = require("./api");
 const app = express()
 
 //Middlewares
+const csrfProtection = csrf({ cookie: true });
+
 app.use(express.json());
+app.use(helmet());
+app.use(csrfProtection);
 
 //chamada das rotas na api
 app.use("/api", apiRoutes);
+
+// enviar o token CSRF ao cliente
+app.get("/api/csrf-token", (req, res) => {
+    res.json({ csrfToken: req.csrfToken() });
+});
 
 //conectando com o banco de dados
 db.connect()
